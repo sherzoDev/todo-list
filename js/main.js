19c4e68e02f9b4a31c467da26f8d87fff71b552c
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-12-03 23:20:35 +0500

    Intial commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const $form = document.querySelector(".form");
+const $int = document.querySelector(".int");
+const $btn = document.querySelector(".btn");
+const $box = document.querySelector(".listBox");
+const $list = document.querySelector(".list");
+const $btns = document.querySelector(".btns");
+const $inputs = document.querySelector(".inputs");
+
+let $arr = [];
+
+
+$form.addEventListener("submit", function(evt) {
+    
+});
+
+let json = JSON.stringify(users);
