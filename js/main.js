1c39bea010410e7eea0b375a9255ac333ccb9662
sherzoDev <mustafoqulovsherzodbek@gmail.com>
2022-12-03 23:51:07 +0500

    Add list

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 const $form = document.querySelector(".form");
 const $int = document.querySelector(".int");
 const $btn = document.querySelector(".btn");
-const $box = document.querySelector(".listBox");
-const $list = document.querySelector(".list");
-const $btns = document.querySelector(".btns");
-const $inputs = document.querySelector(".inputs");
+const $list = document.querySelector(".js-list");
+const $unComplated = document.querySelector(".js-uncomplate-number");
+const $complated = document.querySelector(".js-complate-number");
+const $all = document.querySelector('.js-all-number');
 
 let $arr = [];
+let $intialId = 0;
+let $todos = [];
 
 
-$form.addEventListener("submit", function(evt) {
-    
-});
+const renderArray = function(array, element) {
+    $list.innerHTML = '';
+    const complatedArray = array.filter(function (item) {
+        return item.isCompleted;
+    })
+
+    $complated.textContent = array.length - complatedArray.length;
+    $all.textContent = array.length;
+
+    array.forEach(element => {
+        const $cloneTemp = document.querySelector
+    });
+
+    $form.addEventListener("submit", function(evt) {
+        evt.preventDefault();
+    });
+}
 
 let json = JSON.stringify(users);
